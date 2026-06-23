e312532c99e489983820958c98feabda002a9388
Muhammad Bilal <muhammadbilalbluch@gmial.com>
2026-06-23 12:26:04 +0500

    Add JavaScript conversion operations practice file

--- 01-Basics/03_conversionOperations.js
+++ 01-Basics/03_conversionOperations.js
@@ -49,9 +49,9 @@
  console.log(booleanIsLoggedIn);
 
 
- 1 => True
- 0 => False
- "Bilal" => True
+//  1 => True
+//  0 => False
+//  "Bilal" => True
  
 
 
@@ -62,3 +62,42 @@
  console.log(typeof(stringNumber));
  
  
+// ****************************** Operations ******************************************//
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1 = "Hello "
+let str2 = "Muhammad Bilal"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3 + 4)* 5 % 3);
+console.log(true);
+console.log(+true);
+// console.log(true+);
+console.log(+"");
+num1 = num2 = num3 = 2 + 2
+
+
+
+
+let gameCounter = 100 
+gameCounter++;
+--gameCounter;
+console.log(gameCounter);
