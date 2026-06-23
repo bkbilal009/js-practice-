9b82c88f8350a5590664ad86e118a6fa5432e9e5
Muhammad Bilal <muhammadbilalbluch@gmial.com>
2026-06-23 23:18:38 +0500

    Add JavaScript conversion operations practice file

--- 01-Basics/data_types_summary.js
+++ 01-Basics/data_types_summary.js
@@ -44,3 +44,32 @@ console.log(typeof score);
 console.log(typeof isLoggedIn);
 console.log(typeof outSideTemp);
 console.log(typeof userEmail);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack(Primitive)--->copy|| and Heap memory(Non-Primitive)-->Referance
+
+
+
+let myYouTubeName = "CodeWithBilal"
+let anotherName = myYouTubeName
+
+anotherName = "CSwithBilal"
+
+console.log(myYouTubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@bl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "muhammadbilalbluch@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
