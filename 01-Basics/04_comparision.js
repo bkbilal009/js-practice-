4e5969fc520af671fa06fb3b37ff55c6cd37e294
Muhammad Bilal <muhammadbilalbluch@gmial.com>
2026-06-23 22:17:08 +0500

    Add JavaScript conversion operations practice file

---
+++ 01-Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === ---> strick check || check value & data type
+
+console.log("2" === 2);
+
+
+
+
