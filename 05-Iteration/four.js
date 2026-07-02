fd406d943841de7f5c27d87a7d43b102830700a0
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-02 23:09:42 +0500

    Create four.js

---
+++ 05-Iteration/four.js
@@ -0,0 +1,35 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'cpp',
+    rb: 'ruby',
+    swift : 'Swift by apple'
+}
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+    
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+// }
+
+
+const programming = ["js","py","java","cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+    console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
