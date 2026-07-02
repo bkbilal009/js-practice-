cca82735dce00ba5208e5017dcc0ba4cf6379253
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-02 14:46:01 +0500

    Create switch.js

---
+++ 04-Control flow/switch.js
@@ -0,0 +1,67 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let month = 3;
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("mar");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 6:
+//         console.log("june");
+//         break;
+
+//     default:
+//         console.log("Default case match");
+        
+//         break;
+// }
+
+
+
+
+let month = "March";
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("feb");
+        break;
+    case "March":
+        console.log("mar");
+        break;
+    case "April":
+        console.log("april");
+        break;
+    case "May":
+        console.log("may");
+        break;
+    case "June":
+        console.log("june");
+        break;
+
+    default:
+        console.log("Default case match");
+        
+        break;
+}
